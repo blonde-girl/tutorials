0e38117c65d92815b166f1c0e0b36a3f6c27ed4e
Blonde <35300292+blonde-girl@users.noreply.github.com>
2018-08-10 15:45:10 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 10 15:42:48 2018
+
+@author: Tanya
+"""
+import numpy as np
+import pandas as pd
+from sklearn import cluster, preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import DBSCAN
+
+
+print('Hello world!')
